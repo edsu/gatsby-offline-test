06778499663a79cc7d79c9200cff10700af0e3a1
Ed Summers <ehs@pobox.com>
2021-06-22 13:35:02 -0400

    test

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    title: "Food",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-offline"
+  ],
+};
---
+++ src/pages/hi.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+// markup
+const HiPage = () => {
+  return (
+    <main>
+      Hi
+    </main>
+  )
+}
+
+export default HiPage
